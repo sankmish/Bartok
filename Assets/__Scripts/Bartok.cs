12190984c960309a288b4235412305c3d1a1cbb7
sankmish <mishra.sancho@gmail.com>
2018-07-19 03:32:55 -0400

    Bartok, CardBartok, and Bartok Layout
    Created Bartok, CardBartok and Bartok Layout Scripts. Changed Prefabs.

---
+++ Assets/__Scripts/Bartok.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bartok : MonoBehaviour {
+    static public Bartok S;
+
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+    public TextAsset layoutXML;
+    public Vector3 layoutCenter = Vector3.zero;
+
+    [Header("Set Dynamically")]
+    public Deck deck;
+    public List<CardBartok> drawPile;
+    public List<CardBartok> discardPile;
+
+    private BartokLayout layout;
+    private Transform layoutAnchor;
+
+	// Use this for initialization
+	void Awake () {
+        S = this;
+	}
+	
+	// Update is called once per frame
+	void Start () {
+        deck = GetComponent<Deck>();
+        deck.InitDeck(deckXML.text);
+        Deck.Shuffle(ref deck.cards);
+
+        layout.GetComponent<BartokLayout>();
+        layout.ReadLayout(layoutXML.text);
+
+        drawPile = UpgradeCardsList(deck.cards);
+	}
+
+    List<CardBartok> UpgradeCardsList(List<Card> lCD)
+    {
+        List<CardBartok> lCB = new List<CardBartok>();
+        foreach (Card tCD in lCD)
+        {
+            lCB.Add(tCD as CardBartok);
+        }
+        return (lCB);
+    } 
+}
--- Assets/__Scripts/BartokLayout.cs
+++ Assets/__Scripts/BartokLayout.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*[System.Serializable]
+[System.Serializable]
 public class SlotDef
 {
     public float x;
@@ -12,32 +12,38 @@ public class SlotDef
     public int layerID = 0;
     public int id;
     public List<int> hiddenBy = new List<int>();
+    public float rot;
     public string type = "slot";
-    public Vector3 stagger;
-} */
+    public Vector2 stagger;
+    public int player;
+    public Vector3 pos;
+}
 
-public class Layout : MonoBehaviour {
+public class BartokLayout : MonoBehaviour {
+    [Header("Set Dynamically")]
     public PT_XMLReader xmlr;
     public PT_XMLHashtable xml;
     public Vector2 multiplier;
+
     public List<SlotDef> slotDefs;
     public SlotDef drawPile;
     public SlotDef discardPile;
-
-    public string[] sortingLayerNames = new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw" };
+    public SlotDef target;
 
     public void ReadLayout(string xmlText)
     {
         xmlr = new PT_XMLReader();
         xmlr.Parse(xmlText);
         xml = xmlr.xml["xml"][0];
+
         multiplier.x = float.Parse(xml["multiplier"][0].att("x"));
-        multiplier.y = float.Parse(xml["multiplier"][0].att("y"));
+        multiplier.y = float.Parse(xml["mulitplier"][0].att("y"));
 
         SlotDef tSD;
+
         PT_XMLHashList slotsX = xml["slot"];
 
-        for (int i = 0; i < slotsX.Count; i++)
+        for (int i=0; i<slotsX.Count; i++)
         {
             tSD = new SlotDef();
             if (slotsX[i].HasAtt("type"))
@@ -50,24 +56,14 @@ public class Layout : MonoBehaviour {
 
             tSD.x = float.Parse(slotsX[i].att("x"));
             tSD.y = float.Parse(slotsX[i].att("y"));
-            tSD.layerID = int.Parse(slotsX[i].att("layer"));
+            tSD.pos = new Vector3(tSD.x * multiplier.x, tSD.y * multiplier.y, 0);
 
-            tSD.layerName = sortingLayerNames[tSD.layerID];
+            tSD.layerID = int.Parse(slotsX[i].att("layer"));
+            tSD.layerName = tSD.layerID.ToString();
 
             switch (tSD.type)
             {
                 case "slot":
-                    tSD.faceUp = (slotsX[i].att("faceup") == "1");
-                    tSD.id = int.Parse(slotsX[i].att("id"));
-                    if (slotsX[i].HasAtt("hiddenby"))
-                    {
-                        string[] hiding = slotsX[i].att("hiddenby").Split(',');
-                        foreach (string s in hiding)
-                        {
-                            tSD.hiddenBy.Add(int.Parse(s));
-                        }
-                    }
-                    slotDefs.Add(tSD);
                     break;
 
                 case "drawpile":
@@ -78,11 +74,23 @@ public class Layout : MonoBehaviour {
                 case "discardpile":
                     discardPile = tSD;
                     break;
+
+                case "target":
+                    target = tSD;
+                    break;
+
+                case "hand":
+                    tSD.player = int.Parse(slotsX[i].att("player"));
+                    tSD.rot = float.Parse(slotsX[i].att("rot"));
+                    slotDefs.Add(tSD);
+                    break;
+
             }
 
         }
 
     }
+
+
+
 }
-
-
